b0ac1442727189419a51355366b64490e553cd90
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-08-23 12:59:08 +0300

    Updated project structure. Created Joi schemas for user and todo tasks

---
+++ db.js
@@ -0,0 +1,29 @@
+export const User = {
+  id: '',
+  mail: '',
+  password: '',
+  avatar: '',
+};
+export const dashboard = {
+  id: '',
+  title: '',
+  icon: '',
+  bg: '',
+  columns: [],
+  owner: '',
+};
+export const Column = {
+  id: '',
+  title: '',
+  cards: [],
+  dashboardId: '',
+};
+export const Card = {
+  id: '',
+  columnId: '',
+  dashboardId: '',
+  title: '',
+  description: '',
+  labelColor: '',
+  deadline: '',
+};
