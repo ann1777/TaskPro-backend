14616ab5e7fc2fffa3c95dc00598fcc2b39d0102
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-08-25 13:11:53 +0300

    Send-email router added to the dashboard-router.js. Teat API interface added to the index.html.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import nodemailer from 'nodemailer';
 import bodyParser from 'body-parser';
 import 'dotenv/config';
 import authRouter from './routes/api/auth-router.js';
-import todoRouter from './routes/api/todo-router.js';
+import dashboardRouter from './routes/api/dashboard-router.js';
 
 const app = express();
 
@@ -18,34 +18,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.use('/api/auth', authRouter);
-app.use('/api/dashboard', todoRouter);
-app.post('send-email', async (req, res) => {
-  const { comment, sendEmail } = req.body;
-
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'bc52node@gmail.com',
-      pass: 'justdo1t',
-    },
-  });
-
-  const mailOptions = {
-    from: 'YOUR_EMAIL@gmail.com',
-    to: 'taskpro.project@gmail.com',
-    subject: 'Help Request',
-    text: `User Email: ${userEmail}\nComment: ${comment}`,
-  };
-
-  try {
-    await transporter.sendMail(mailOptions);
-    console.log('Email sent successfully!');
-    res.status(200).send('Email sent successfully!');
-  } catch (error) {
-    console.error('Error sending email:', error);
-    res.status(500).send('Error sending email.');
-  }
-});
+app.use('/api/dashboard', dashboardRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
--- routes/api/dashboard-router.js
+++ routes/api/dashboard-router.js
@@ -1,38 +1,185 @@
-import express from "express";
-import dashboardSchema from "../../schemas/dashboard-schemas.js";
-import dashboardController from "../../controlers/dashboard-controller.js";
+// import express from 'express';
+// import dashboardSchema from '../../schemas/dashboard-schemas.js';
+// import dashboardController from '../../controllers/dashboard-controller.js';
 
-import {
-  isEmptyBody,
-  isValidId,
-  authenticate,
-  upload,
-} from "../../middlewares/index.js";
+// import {
+//   isEmptyBody,
+//   isValidId,
+//   authenticate,
+//   upload,
+// } from '../../middlewares/index.js';
+// import validateBody from '../../decorators/validateBody.js';
+// console.log(
+//   `upload :`,
+//   upload,
+//   `\nisValidId :`,
+//   isValidId,
+//   `\nisEmptyBody :`,
+//   isEmptyBody,
+//   `\nauthenticate :`,
+//   authenticate
+// );
+
+// const dashboardRouter = express.Router();
+
+// const dashboardAddValidate = validateBody(dashboardSchema.dashboardAddSchema);
+
+// dashboardRouter.use(authenticate);
+
+// dashboardRouter.get('/', dashboardController.getAll);
+// dashboardRouter.get('/:dashboardId', isValidId, dashboardController.getById);
+// dashboardRouter.post(
+//   '/',
+//   isEmptyBody,
+//   dashboardAddValidate,
+//   dashboardController.add
+// );
+
+// dashboardRouter.post('/helpMail', async (req, res) => {
+//   const { comment, userEmail } = req.body;
+
+//   const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: 'bc52node@gmail.com',
+//       pass: 'justdo1t',
+//     },
+//   });
+
+// const mailOptions = {
+//   from: 'bc52node@gmail.com',
+//   to: 'taskpro.project@gmail.com',
+//   subject: 'Help Request',
+//   text: `User Email: ${userEmail}\nComment: ${comment}`,
+// };
+//   console.log(
+//     `mailOptions :`,
+//     mailOptions,
+//     `\nuserEmail :`,
+//     userEmail,
+//     `\ncomment :`,
+//     comment
+//   );
+
+//   try {
+//     await transporter.sendMail(mailOptions);
+//     console.log('Email sent successfully!');
+//     res.status(200).send('Email sent successfully!');
+//   } catch (error) {
+//     console.error('Error sending email:', error);
+//     res.status(500).send('Error sending email.');
+//   }
+// });
+
+// dashboardRouter.delete(
+//   '/:dashboardId',
+//   isValidId,
+//   dashboardController.deleteById
+// );
+
+// dashboardRouter.put(
+//   '/:dashboardId',
+//   isValidId,
+//   isEmptyBody,
+//   dashboardAddValidate,
+//   dashboardController.updateById
+// );
+// export default dashboardRouter;
+
+import express from 'express';
+const router = express.Router();
+import User from '../../models/user.js';
+import { google } from 'googleapis';
+const credentials = process.env; // Replace with the actual path to your credentials JSON file
 
 const dashboardRouter = express.Router();
+const SCOPES = ['https://www.googleapis.com/auth/gmail.send'];
 
-const dashboardAddValidate = validateBody(dashboardSchema.dashboardAddSchema);
+// Load credentials and other configuration from environment variables
+const { GMAIL_API_CLIENT_ID, JWT_SECRET, BASE_URL } = process.env;
 
-dashboardRouter.use(authenticate);
-
-dashboardRouter.get("/", dashboardController.getAll);
-dashboardRouter.get("/:dashboardId", isValidId, dashboardController.getById);
-dashboardRouter.post(
-  "/",
-  isEmptyBody,
-  dashboardAddValidate,
-  dashboardController.add
-);
-dashboardRouter.delete(
-  "/:dashboardId",
-  isValidId,
-  dashboardController.deleteById
+// Create a new OAuth2 client
+const oAuth2Client = new google.auth.OAuth2(
+  GMAIL_API_CLIENT_ID,
+  '', // Replace this with your client secret
+  `${BASE_URL}/auth-callback` // Replace with your OAuth2 redirect URL
 );
 
-dashboardRouter.put(
-  "/:dashboardId",
-  isValidId,
-  isEmptyBody,
-  dashboardAddValidate,
-  dashboardController.updateById
-);
+// const TOKEN_PATH = 'token.json'; // Replace with your token file path
+// Middleware to authenticate user
+const authenticate = async (req, res, next) => {
+  const { authorization = '' } = req.headers;
+  const [bearer, token] = authorization.split(' ');
+
+  if (bearer !== 'Bearer') {
+    throw new HttpError(401);
+  }
+
+  try {
+    const { id } = jwt.verify(token, JWT_SECRET);
+    const user = await User.findById(id);
+
+    if (!user || !user.token) {
+      throw new HttpError(401);
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new HttpError(401, error.message);
+  }
+};
+
+// Check if we have previously stored a token.
+// fs.readFile(TOKEN_PATH, (err, token) => {
+//   if (err) {
+//     // If token doesn't exist, get new token by following the OAuth2 flow
+//     getAccessToken(oAuth2Client);
+//   } else {
+//     oAuth2Client.setCredentials(JSON.parse(token));
+//   }
+// });
+
+// Function to get an access token
+const getAccessToken = () => {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  return authUrl;
+};
+
+// Route to send an email
+dashboardRouter.post('/send-email', authenticate, async (req, res) => {
+  const { base64EncodedEmail } = req.body;
+
+  if (!base64EncodedEmail) {
+    return res.status(400).json({ error: 'Email content is missing' });
+  }
+
+  try {
+    oAuth2Client.setCredentials(req.user.token);
+
+    const mailOptions = {
+      from: 'bc52node@gmail.com',
+      to: 'taskpro.project@gmail.com',
+      subject: 'Help Request',
+      text: `User Email: bc52node@gmail.com\nComment: ${comment}`,
+    };
+    const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+    const response = await gmail.users.messages.send({
+      userId: 'me',
+      resource: {
+        raw: base64EncodedEmail,
+        mailOptions,
+      },
+    });
+    console.log('Email sent:', response.data);
+    res.json({ message: 'Email sent successfully' });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).json({ error: 'Failed to send email' });
+  }
+});
+
+export default dashboardRouter;
