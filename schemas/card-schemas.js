f1f644305b9aed628aa5697cf8e711533cb6f20d
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-08-27 19:21:08 +0300

    Added cards and columns operations descriptions to the swagger.js. Updated card-schemas.js and column-chemas.js

--- schemas/card-schemas.js
+++ schemas/card-schemas.js
@@ -1,14 +1,23 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 export const cardAddSchema = Joi.object({
   title: Joi.string().required(),
   description: Joi.string(),
   priority: Joi.string(),
   deadline: Joi.string(),
+});
+
+export const cardUpdateSchema = Joi.object({
+  cardId: Joi.string().required(),
+  title: Joi.string(),
+  dashboardId: Joi.string(),
+  description: Joi.string(),
+  priority: Joi.string(),
+  deadline: Joi.string(),
   dashboardId: Joi.string(),
   columnId: Joi.string(),
-  cards: Joi.array(),
 });
+
 export default {
   cardAddSchema,
 };
--- schemas/column-schemas.js
+++ schemas/column-schemas.js
@@ -1,10 +1,18 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 export const columnAddSchema = Joi.object({
   title: Joi.string().required(),
   dashboardId: Joi.string(),
   cards: Joi.array(),
 });
+
+export const columnUpdateSchema = Joi.object({
+  title: Joi.string().required(),
+  dashboardId: Joi.string(),
+  columnId: Joi.string(),
+  cards: Joi.array(),
+});
+
 export default {
   columnAddSchema,
 };
