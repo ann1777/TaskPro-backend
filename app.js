25bdb2a7dbcd0119fe9923d36905558ae8347642
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-08-30 10:04:53 +0300

    Added '/api-docs' route to the app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-import express from "express";
-import logger from "morgan";
-import cors from "cors";
+import express from 'express';
+import logger from 'morgan';
+import cors from 'cors';
 import bodyParser from 'body-parser';
-import "dotenv/config";
-import authRouter from "./routes/api/auth-router.js";
-import dashboardRouter from "./routes/api/dashboard-router.js";
-import columnRouter from "./routes/api/column-router.js";
-import cardRouter from "./routes/api/card-router.js";
+import 'dotenv/config';
+import authRouter from './routes/api/auth-router.js';
+import dashboardRouter from './routes/api/dashboard-router.js';
+import columnRouter from './routes/api/column-router.js';
+import cardRouter from './routes/api/card-router.js';
 
 const app = express();
 
@@ -18,10 +18,11 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.use("/api/auth", authRouter);
-app.use("/api/dashboard", dashboardRouter);
-app.use("/api/column/", columnRouter);
-app.use("/api/card/", cardRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/dashboard', dashboardRouter);
+app.use('/api/column/', columnRouter);
+app.use('/api/card/', cardRouter);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
