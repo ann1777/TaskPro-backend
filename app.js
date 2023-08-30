be69e0dd013391fa0381cb1fb7797f9e7ed2e07c
Angela Potapchuk <75266386+ann1777@users.noreply.github.com>
2023-08-30 10:43:45 +0300

    Installer swagger-ui package. Added swagger.json

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import swaggerUI from 'swagger-ui';
 import 'dotenv/config';
 import authRouter from './routes/api/auth-router.js';
 import dashboardRouter from './routes/api/dashboard-router.js';
@@ -22,7 +23,14 @@ app.use('/api/auth', authRouter);
 app.use('/api/dashboard', dashboardRouter);
 app.use('/api/column/', columnRouter);
 app.use('/api/card/', cardRouter);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
+
+// import('./swagger.json')
+//   .then((specs) => {
+//     app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
+//   })
+//   .catch((error) => {
+//     console.error('Error loading Swagger specs:', error);
+//   });
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
